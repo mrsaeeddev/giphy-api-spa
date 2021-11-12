9c1644d7ebf66643a87f06c8cf9b470370117c42
mrsaeeddev <saeed_dev@yahoo.com>
2021-11-12 15:10:43 +0500

    added more tests for thumbnails component

--- src/components/Thumbnails/Thumbnails.test.tsx
+++ src/components/Thumbnails/Thumbnails.test.tsx
@@ -3,10 +3,54 @@ import Thumbnails from './Thumbnails';
 
 describe('Thumbnails Component', () => {
     it('renders Thumbnails component correctly', () => {
-        let data: any[] = [{ url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4" }, {
-            url: "https://giphy.com/gifs/tongue-goodnight-sJWNLTclcvVmw"
+        let data: any[] = [{
+            images: {
+                fixed_height: {
+                    url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+                },
+            },
+            url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
         }];
         mount(<Thumbnails data={data} />);
         cy.get('div').should('have.class', 'row')
+    });
+
+    it('renders images correctly', () => {
+        let data: any[] = [{
+            images: {
+                fixed_height: {
+                    url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+                },
+            },
+            url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+        }];
+        mount(<Thumbnails data={data} />);
+        cy.get('img').should('have.attr', 'src');
+    });
+
+    it('add relevant CSS class correctly', () => {
+        let data: any[] = [{
+            images: {
+                fixed_height: {
+                    url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+                },
+            },
+            url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+        }];
+        mount(<Thumbnails data={data} />);
+        cy.get('div').should('have.class', 'gif-wrap');
+    });
+
+    it('renders the grid correctly', () => {
+        let data: any[] = [{
+            images: {
+                fixed_height: {
+                    url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+                },
+            },
+            url: "https://giphy.com/gifs/mlb-los-angeles-angels-laa-fHsweg1kp5uzU3G1D4"
+        }];
+        mount(<Thumbnails data={data} />);
+        cy.get('div').should('have.class', 'col-lg-4 col-md-4 col-12');
     });
 });

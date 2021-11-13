0c4263facfcdabaa5bb19a5337f9d1025b3deae1
mrsaeeddev <saeed_dev@yahoo.com>
2021-11-13 13:57:56 +0500

    added background color to images and autoplay attribute to videos

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -4,7 +4,7 @@ import { ImageProps } from "./Image.interface";
 function Image(props: ImageProps) {
     return (
         <div className="gif-wrap">
-            <img src={props.src} onClick={props.onClick} alt="GIPHY GIFs" />
+            <img src={props.src} onClick={props.onClick} alt="GIPHY GIFs" loading="lazy" />
         </div>)
 }
 
--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -2,7 +2,7 @@ import { VideoProps } from "./Video.interface";
 
 function Video(props: VideoProps) {
     return (
-        <video width="100%" height="300px" src={props.src} />
+        <video width="100%" height="300px" src={props.src} autoPlay loop />
     )
 }
 
